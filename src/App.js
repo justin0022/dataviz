a0ad74c825a6f418d9d234e009243e86814a7054
Justin Lee <justinlee0022@gmail.com>
2018-12-18 08:56:40 -0800

    Changed react version to alpha, trying out hooks. LOVE IT

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import Home from './routes/Home'
-import LineChart from './routes/LineChart'
-import BarChart from './routes/BarChart'
+import Home from './containers/Home'
+import ChartsContainer from './containers/ChartsContainer'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 function App () {
@@ -13,20 +12,12 @@ function App () {
             <Link to='/'>Home</Link>
           </li>
           <li>
-            <Link to='/BarChart'>Bar Chart</Link>
-          </li>
-          <li>
-            <Link to='/LineChart'>Line Chart</Link>
+            <Link to='/Charts'>Bar Chart</Link>
           </li>
         </ul>
-
         <hr />
-
         <Route exact path='/' component={Home} />
-        <div className='parent'>
-          <Route path='/BarChart' className='chart' component={BarChart} />
-        </div>
-        <Route path='/LineChart' component={LineChart} />
+        <Route path='/Charts' component={ChartsContainer} />
       </div>
     </Router>
   )
---
+++ src/containers/ChartsContainer.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+
+const ChartsContainer = () => {
+  const [data] = useState()
+
+  useEffect(() => {
+    // data = 
+  })
+
+  return (
+    <h2>hello</h2>
+  )
+}
+
+export default ChartsContainer
