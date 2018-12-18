7be136085ffd44c43dd200a56f2bb82df446debf
Justin Lee <justinlee0022@gmail.com>
2018-12-18 09:21:02 -0800

    Working on integrating material design components into app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import AppBar from '@material-ui/core/AppBar';
 import Home from './containers/Home'
 import ChartsContainer from './containers/ChartsContainer'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 function App () {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/Charts'>Bar Chart</Link>
-          </li>
-        </ul>
-        <hr />
-        <Route exact path='/' component={Home} />
-        <Route path='/Charts' component={ChartsContainer} />
-      </div>
-    </Router>
+    <div>
+      <CssBaseline />
+      <AppBar position='absolute'>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/Charts'>Bar Chart</Link>
+              </li>
+            </ul>
+            <hr />
+            <Route exact path='/' component={Home} />
+            <Route path='/Charts' component={ChartsContainer} />
+          </div>
+        </Router>
+      </AppBar>
+    </div>
   )
 }
 
